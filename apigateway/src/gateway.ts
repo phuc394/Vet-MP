82b384cc42a37d0eaf615ca1f637a7964bc299af
phuc394 <phuc.thg02394@sinhvien.hoasen.edu.vn>
2026-04-29 20:37:35 +0700

    Fix: Apply pathRewrite to each service

--- apigateway/src/gateway.ts
+++ apigateway/src/gateway.ts
@@ -7,47 +7,74 @@ const port = Number(process.env.PORT) || 3000;
 app.use('/api/v1/auth', createProxyMiddleware({
     target: 'http://localhost:3001',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/auth': '/auth',
+    },
 }));
 
 app.use('/api/v1/users', createProxyMiddleware({
     target: 'http://localhost:3001',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/users': '/users',
+    },
 }));
 
 app.use('/api/v1/pets', createProxyMiddleware({
     target: 'http://localhost:3002',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/pets': '/pets',
+    },
 }));
 
 app.use('/api/v1/catalog', createProxyMiddleware({
     target: 'http://localhost:3003',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/catalog': '/catalog',
+    },
 }));
 
 app.use('/api/v1/inventory', createProxyMiddleware({
     target: 'http://localhost:3004',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/inventory': '/inventory',
+    },
 }));
 
 app.use('/api/v1/suppliers', createProxyMiddleware({
     target: 'http://localhost:3004',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/suppliers': '/suppliers',
+    },
 }));
 
 app.use('/api/v1/appointments', createProxyMiddleware({
     target: 'http://localhost:3005',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/appointments': '/appointments',
+    },
 }));
 
 app.use('/api/v1/medical-records', createProxyMiddleware({
     target: 'http://localhost:3006',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/medical-records': '/medical-records',
+    },
 }));
 
 
 app.use('/api/v1/reports', createProxyMiddleware({
     target: 'http://localhost:3007',
     changeOrigin: true,
+    pathRewrite: {
+        '^/api/v1/reports': '/reports',
+    },
 }));
 
 app.listen(port, () => {
