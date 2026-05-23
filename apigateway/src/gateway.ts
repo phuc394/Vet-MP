91a013ded21d9abf5026d5f148ce14d3ab0709a4
phuc394 <phuc.thg02394@sinhvien.hoasen.edu.vn>
2026-05-23 11:05:54 +0700

    Commit again

--- apigateway/src/gateway.ts
+++ apigateway/src/gateway.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
